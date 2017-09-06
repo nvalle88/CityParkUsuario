eafe2598fe6ae5b2ff0874b45fc4a692b067d9aa
Brian Molina <bmolina@digitalstrategy.com.ec>
2017-09-06 12:36:05 -0500

    Cambios en la UI para poder ver la barra y los iconos

--- AppParqueoAzul/AppParqueoAzul/AppParqueoAzul/Pages/MainPage.xaml.cs
+++ AppParqueoAzul/AppParqueoAzul/AppParqueoAzul/Pages/MainPage.xaml.cs
@@ -16,43 +16,46 @@ namespace AppParqueoAzul.Pages
         ApiService apiService = new ApiService();
 
         Double tr = 0;
+        Double tiempoLabel = 0;
         public MainPage()
         {
             InitializeComponent();
+            //ImagenCity.IsVisible = true;
             loadTime();
-
         }
+
         private async void loadTime()
         {
             var tiempo = await apiService.ConsultarTiempo(new UsuarioRequest { UsuarioId = App.UsuarioActual.UsuarioId.ToString(), });
-            if (tiempo.Restante > new TimeSpan(0))
+            if (tiempo.Restante > new TimeSpan(0) && tiempo!=null)
             {
+                ProgressBarTime.IsVisible = true;
+                ImagenCity.IsVisible = false;
               Debug.WriteLine("si tiene tiempo disponible, {0}", tiempo.Restante);
               Double tiempoRestante = (tiempo.Restante.Hours + tiempo.Restante.Minutes / 100.0 + tiempo.Restante.Seconds / 10000.0) * (tiempo.Restante > TimeSpan.Zero ? 1 : -1);
               Double  tiempoComprado = (tiempo.Comprado.Hours + tiempo.Comprado.Minutes / 100.0 + tiempo.Comprado.Seconds / 10000.0) * (tiempo.Comprado > TimeSpan.Zero ? 1 : -1);
-
-                tr = ((100 / tiempoComprado) * tiempoRestante)/ 100;
+                tr = ((100 / tiempoComprado) * tiempoRestante)/ 100;               
                 Debug.WriteLine(tr);
+                tiempoLabel = Convert.ToInt32(tiempoRestante * 100);
                 ProgressTime.Progress = 1-tr;
-                LabelRestante.Text = Convert.ToInt32(tr * 100) + "  min restante";
+                LabelRestante.Text = tiempoLabel + "  min restante";
                 Xamarin.Forms.Device.StartTimer(TimeSpan.FromMinutes(1), OnTimer);
-
             }
             else
             {
+                ProgressBarTime.IsVisible = false;
+                ImagenCity.IsVisible = true;
                 Debug.WriteLine("no tiene tiempo disponible, {0}", tiempo.Restante);
             }
-
-
         }
         private bool OnTimer()
-        {          
+        {
+            tiempoLabel = tiempoLabel - 1;
+            LabelRestante.Text = tiempoLabel + "  min restante";
 
             var progress = (ProgressTime.Progress + .01);
-            if (progress < 1) progress = 0;
-            ProgressTime.Progress = progress;
-            LabelRestante.Text = (int.Parse((tr*100).ToString())+"  min restante");
-
+            if (progress > 1) progress = 0;
+            ProgressTime.Progress = progress;            
             return true;
         }
 
